f3affac9e90fa1e31540b71694ad4524437d45a0
Alexander <a.tselkovnikov@encry.ru>
2019-12-06 17:24:46 +0300

    Добавил список городов, типы точек маршрута и иконки

---
+++ src/const.js
@@ -0,0 +1,23 @@
+export const CITIES = [
+  `Geneva`,
+  `Chamonix`,
+  `Amsterdam`
+];
+
+export const TYPES = [
+  `Taxi to`,
+  `Flight to`,
+  `Check into`
+];
+
+export const ICONS = [
+  `bus`,
+  `check-in`,
+  `drive`,
+  `flight`,
+  `ship`,
+  `taxi`,
+  `train`,
+  `transport`,
+  `trip`
+];
---
+++ src/mock/event.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+const Cities = [
+
+];
+
+const Types = [
+  {
+    name: ``,
+    icon: `bus`
+  },
+];
