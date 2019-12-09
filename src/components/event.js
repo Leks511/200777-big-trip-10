46ade679313534bb6df21b8038575987e6b6687d
Alexander <a.tselkovnikov@encry.ru>
2019-12-09 17:55:52 +0300

    Разделил рендеринг эвентов на разные функции

--- src/components/event.js
+++ src/components/event.js
@@ -10,45 +10,47 @@ const createOfferMarkup = (offers) => {
         </li>
     `);
   }).join(`\n`);
-}
+};
 
-export const createEventTemplate = (count) => {
+const createEventMorkup = () => {
   const { description, photo, type, city, options, eventPeriod, eventDuration, price } = generateEvent();
 
   const offers = createOfferMarkup(options);
 
-  return new Array(count).fill(``).map(() => {
-    return (`
-      <li class="trip-events__item">
-        <div class="event">
-          <div class="event__type">
-            <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
-          </div>
-          <h3 class="event__title">Taxi to airport</h3>
-
-          <div class="event__schedule">
-            <p class="event__time">
-              <time class="event__start-time" datetime="2019-03-18T10:30">${eventPeriod.startTime}</time>
-              &mdash;
-              <time class="event__end-time" datetime="2019-03-18T11:00">${eventPeriod.endTime}</time>
-            </p>
-            <p class="event__duration">${eventDuration}</p>
-          </div>
-
-          <p class="event__price">
-            &euro;&nbsp;<span class="event__price-value">${price}</span>
-          </p>
-
-          <h4 class="visually-hidden">Offers:</h4>
-          <ul class="event__selected-offers">
-            ${offers}
-          </ul>
-
-          <button class="event__rollup-btn" type="button">
-            <span class="visually-hidden">Open event</span>
-          </button>
+  return (`
+    <li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
         </div>
-      </li>
-    `);
-  })
+        <h3 class="event__title">Taxi to airport</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-18T10:30">${eventPeriod.startTime}</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-18T11:00">${eventPeriod.endTime}</time>
+          </p>
+          <p class="event__duration">${eventDuration}</p>
+        </div>
+
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${price}</span>
+        </p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          ${offers}
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>
+  `);
+};
+
+export const createEventListTemplate = (count) => {
+  return new Array(count).fill(``).map(() => createEventMorkup()).join(``);
 };
